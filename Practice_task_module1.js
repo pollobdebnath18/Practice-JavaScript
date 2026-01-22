849b388741cb5735823aeffb407ae2cc083082c8
pollob debnath <pullobdebnath@gmail.com>
2026-01-22 21:33:16 +0600

    average mark

---
+++ Practice_task_module1.js
@@ -0,0 +1,10 @@
+// average mark of 5 subject 
+var math = 75.25;
+var bio = 65.85;
+var chem = 80;
+var phy = 35.45;
+var bang = 99.5;
+
+var total = math + bio + chem + phy + bang;
+var average = (total/5);
+console.log(average.toFixed(2));
