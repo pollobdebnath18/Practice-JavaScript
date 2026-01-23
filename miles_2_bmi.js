4cd87b32a29543abce0e93e93c256b6c62ba9f1d
pollob debnath <pullobdebnath@gmail.com>
2026-01-23 21:19:18 +0600

    bmi calculate

---
+++ miles_2_bmi.js
@@ -0,0 +1,11 @@
+// BMI calculate
+let bmi = 20.1;
+if(bmi<18.5){
+    console.log("Weight is very low means Underweight");
+}
+else if(bmi>=18.5 && bmi<=24.9){
+    console.log("Normal (Healthy weight)");
+}
+else {
+    console.log("Not Normal");
+}
