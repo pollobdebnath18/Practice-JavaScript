dfe05da97e57cfd98e1477562ce1ba21aa9e0532
pollob debnath <pullobdebnath@gmail.com>
2026-01-23 21:23:35 +0600

    check leap year

--- miles_2_bmi.js
+++ miles_2_bmi.js
@@ -1,11 +1,20 @@
 // BMI calculate
 let bmi = 20.1;
-if(bmi<18.5){
-    console.log("Weight is very low means Underweight");
+if (bmi < 18.5) {
+  console.log("Weight is very low means Underweight");
+} else if (bmi >= 18.5 && bmi <= 24.9) {
+  console.log("Normal (Healthy weight)");
+} else {
+  console.log("Not Normal");
 }
-else if(bmi>=18.5 && bmi<=24.9){
-    console.log("Normal (Healthy weight)");
-}
-else {
-    console.log("Not Normal");
+
+// Leap year Check
+
+let year = 1900;
+if (year % 100 != 0 && year % 4 == 0) {
+  console.log("Leap Year");
+} else if (year % 400) {
+  console.log("Leap year");
+} else {
+  console.log("Not Leap year");
 }
